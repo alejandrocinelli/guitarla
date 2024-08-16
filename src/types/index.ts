19d368c81aff92bd3aabceb98885765343dcd17a
Alejandro Osvaldo Cinelli Coria <aocinellicoria@teco.com.ar>
2024-08-16 01:19:06 -0300

    a

---
+++ src/types/index.ts
@@ -0,0 +1,26 @@
+export type Guitar = {
+    id: number
+    name: string
+    image: string
+    description: string
+    price: number
+}
+
+export type CartItem = Guitar & {
+    quantity: number
+}
+
+export type vacuna = {
+    name : string,
+    price : number 
+
+}
+
+// utility types example 
+//export type CartItem = Pick<Guitar, 'id' | 'name' | 'price' > & {
+//     quantity: number
+// }
+// export type CartItem = Omit<Guitar, 'id' | 'name' | 'price' > & {
+//     quantity: number
+// }
+
